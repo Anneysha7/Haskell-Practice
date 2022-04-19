88a29679aad6cba60856a18770141269a1875449
Anneysha Sarkar <usaas23@gmail.com>
2022-04-19 09:31:27 +0530

    Update comp1100-ideas.hs

--- comp1100-ideas.hs
+++ comp1100-ideas.hs
@@ -14,3 +14,20 @@ natToIntHelper num
 natToInt :: [Integer] -> Integer
 natToInt list = length (natToIntHelper list)
 -}
+
+{- ERROR
+Lab06.hs:224:7: error:
+    * No instance for (Eq Nat) arising from a use of `=='
+    * In the expression: num == Z
+      In a stmt of a pattern guard for
+                     an equation for `natToIntHelper':
+        num == Z
+      In an equation for `natToIntHelper':
+          natToIntHelper num
+            | num == Z = []
+            | num /= Z = [1] ++ natToIntHelper (decrement num)
+    |
+224 |     | num == Z = []
+    |       ^^^^^^^^
+Failed, no modules loaded.
+-}
